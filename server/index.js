159d9f6965e6ba4a85b3c62718b22ecde8c815cd
Harsh Lohana <harshylohana15@gmail.com>
2024-02-04 20:26:28 +0530

    initial deployment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const connectToDB = require("./db");
 const userRouter = require("./routes/user.routes");
 const cargoTypeRouter = require("./routes/cargotype.routes");
+const path = require("path");
 
 dotenv.config();
 
@@ -15,14 +16,24 @@ connectToDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.json({
-    msg: "Cargo Connect",
-  });
-});
-
 app.use("/api/user", userRouter);
 app.use("/api/type",cargoTypeRouter);
+
+// ---------- depolyment ----------
+
+const __currdir = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__currdir, "/client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__currdir, "client", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    return res.send("API is running!");
+  });
+}
+
+// ---------- depolyment ----------
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
